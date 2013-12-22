60189f2ee2411ddb04f183d6d65e40a24c9b3b37
messboy <messboy000@gmail.com>
2013-12-22 17:29:25 +0800

    11172 一行一行輸出版

--- 11172.cpp
+++ 11172.cpp
@@ -13,22 +13,27 @@ int main()
     int num = 0;
     while (cin >> num)
     {
-       int size = num*2;
-       vector<long long int> box(size);
-       for (int i=0; i< size; i++)
-       {
-           cin >> box[i];
-       }
+        vector<long long int> box(2);
+//       int size = num*2;
+//      vector<long long int> box(size);
+//       for (int i=0; i< 2; i++)
+//       {
+//           cin >> box[i];
+//       }
 
-       for (int i=0; i<size; i = i+2)
-       {
-           if (box[i] > box[i+1])
+       for (int i=0; i<num; i++)
+        {
+           for (int j=0; j< 2; j++)
+            {
+                cin >> box[j];
+            }
+           if (box[0] > box[1])
             cout << ">\n";
-           else if (box[i] < box[i+1])
+           else if (box[0] < box[1])
             cout << "<\n";
            else
             cout << "=\n";
-       }
+        }
     }
 }
 
